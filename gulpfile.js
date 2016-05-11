cf50d2499d5bf0bad87654503c29ec0a5a995d87
Arne Van den Bogaert <arne.vandenbogaert@district01.be>
2016-05-11 12:06:09 +0200

    add bootstrap CSS + FontAwesome implementation

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+
 var scripts =  [
     './node_modules/datatables.net/js/jquery.dataTables.js',
     './node_modules/datatables.net-bs/js/dataTables.bootstrap.js',
     './assets-src/scripts/init.js'
 ];
+
+var conf = {
+    bootstrap: ['./node_modules/datatables.net-bs/css/*.css', './node_modules/datatables.net-bs/css/*.scss'],
+    fontAwesome: ['./assets-src/sass/datatables_font_awesome.scss']
+};
 
 gulp.task('scripts', function () {
     return gulp.src(scripts, {base: '.'})
@@ -17,4 +25,24 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./src/Phpro/DatatablesBundle/Resources/public/js/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('sass-bootstrap', function(done){
+    gulp.src(conf.bootstrap)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('datatables-bootstrap.css'))
+        .pipe(cleanCss())
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./src/Phpro/DatatablesBundle/Resources/public/css/'))
+        .on('end', done)
+});
+
+gulp.task('sass-font-awesome', function(done){
+    gulp.src(conf.fontAwesome)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('datatables-font-awesome.css'))
+        .pipe(cleanCss())
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./src/Phpro/DatatablesBundle/Resources/public/css/'))
+        .on('end', done)
+});
+
+gulp.task('default', ['scripts', 'sass-bootstrap', 'sass-font-awesome']);
